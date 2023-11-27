0b30c14f65f7389127ed7c7a9d24f45f93da07a7
Manoel <manoel.oliveira@beansoftwares.com.br>
2023-11-27 13:41:52 -0300

    feat: Add a navigation in project

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -1,15 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { styles } from "./styles";
 
-export default function Home({ navigation }: any) {
+export default function About() {
   return (
     <View style={styles.container}>
       <Text>Testando primeira renderização do aphpgvyhvgcg</Text>
-      <Button
-        title="Sobre"
-        onPress={() => navigation.navigate("About")}
-      ></Button>
       <StatusBar style="auto" />
     </View>
   );
